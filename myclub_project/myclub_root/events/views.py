df39ebfd449c0f1827ee8cce4e7665957281700b
prianthon <prianthonsubardio@gmail.com>
2020-10-27 10:53:50 +0700

    dynamic view

--- myclub_project/myclub_root/events/views.py
+++ myclub_project/myclub_root/events/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 def index(request):
-    return HttpResponse("<h1>MyClub Event Calendar</h1>")
+    t = date.today()
+    month = date.strftime(t, '%b')
+    year = t.year
+    title = "MyClub Event Calendar - %s %s" % (month,year)
+    return HttpResponse("<h1>%s</h1>" % title)
