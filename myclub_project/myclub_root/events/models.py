cf46a2f6e7cd65eb38720168f46813df2f7ebdae
prianthon <prianthonsubardio@gmail.com>
2020-10-30 06:03:01 +0700

    optional form venue

--- myclub_project/myclub_root/events/models.py
+++ myclub_project/myclub_root/events/models.py
@@ -4,9 +4,9 @@ class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip/Post Code', max_length=12)
-    phone = models.CharField('Contact Phone', max_length=20)
-    web = models.URLField('Web Assress')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=20, blank=True)
+    web = models.URLField('Web Address', blank=True)
+    email_address = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name
